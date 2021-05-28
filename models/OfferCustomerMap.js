ac47712197911c30f4a35100eeff3a7802ab6704
uzair-bms <uzair@boostmystartup.com>
2021-05-29 01:42:23 +0500

    minor fix

--- models/OfferCustomerMap.js
+++ models/OfferCustomerMap.js
@@ -18,10 +18,6 @@ const OfferCustomerMapSchema = new Schema({
     offer_campName:{
         type: String,
     },
-    business_username: {
-        type: String,
-        default: '',
-    },
     live_date: {
         type: Date,
         default: Date.now
